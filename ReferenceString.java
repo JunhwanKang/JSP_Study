3bd672a58da97d400e94c89fd21ad4a39989ab47
JunhwanKang <wnsghks1017@naver.com>
2020-10-08 22:12:16 +0900

    자바 공부

---
+++ ReferenceString.java
@@ -0,0 +1,12 @@
+package javaStudy;
+
+public class ReferenceString {
+	public ReferenceString() {}
+	public ReferenceString(String arg) {
+	
+	}
+	public static void main(String[] args) {
+		ReferenceString reference = new ReferenceString();
+		
+	}
+}
---
+++ for_each.java
@@ -0,0 +1,11 @@
+package javaStudy;
+
+public class for_each {
+	public static void main(String[] args) {
+		int[] ar = {1,2,3,4,5,6,7,8,9};
+		for(int data: ar) {
+			System.out.println(data);
+		}
+	}
+	
+}
