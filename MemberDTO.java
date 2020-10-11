8492fbb6ab021e21a2338aec41138d461e141550
JunhwanKang <wnsghks1017@naver.com>
2020-10-11 20:56:05 +0900

    pass by value/ pass by reference

---
+++ MemberDTO.java
@@ -0,0 +1,26 @@
+package javaStudy;
+
+public class MemberDTO {
+	public String name;
+	public String phone;
+	public String email;
+
+	public MemberDTO() {
+		//	 아무 정보도 모를 때
+	}
+
+	public MemberDTO(String name) {
+		this.name = name;  //이름만알 때
+	}
+	
+	public MemberDTO(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+	}
+	
+	public MemberDTO(String name, String phone, String email) {
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
+}
