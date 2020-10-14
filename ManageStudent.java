379d392235cb99028abbaf40a4ec978faaa97609
JunhwanKang <wnsghks1017@naver.com>
2020-10-14 19:04:48 +0900

    toString / hashCode overriding

--- ManageStudent.java
+++ ManageStudent.java
@@ -2,10 +2,21 @@ package javaStudy;
 
 public class ManageStudent {
 	public static void main(String[] args) {
-		Student[] student = null;
-		
+//		Student[] student = null;
+//		
+		ManageStudent mst = new ManageStudent();
 		Student st = new Student();
-		student = st.addStudent();
-		st.printStudent(student);
+//		student = st.addStudent();
+//		st.printStudent(student);
+		mst.checkEquals();
+	}
+	public void checkEquals() {
+		Student a = new Student("Min", "Seoul", "010xxxxxxxx", "ask@goodjava.com");
+		Student b = new Student("Min", "Seoul", "010xxxxxxxx", "ask@goodjava.com");
+		
+		if(a.equals(b))
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
 	}
 }
